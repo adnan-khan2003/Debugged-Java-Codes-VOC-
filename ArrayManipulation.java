4b47219dc668a0443bb65d2eb89a10a886fd7acb
ADNAN KHAN <134621053+adnan-khan2003@users.noreply.github.com>
2023-09-04 18:27:20 +0530

    ArrayManipulation.java

--- ArrayManipulation.java
+++ ArrayManipulation.java
@@ -9,7 +9,7 @@ within the bounds of the array.
 public class ArrayManipulation {
         public static void main(String[] args) {
             int[] numbers = {1, 2, 3, 4, 5};
-//          I have changed the condition of For loop from (i <= numbers.length) to (i < numbers.length)
+            //I have changed the condition of For loop from (i <= numbers.length) to (i < numbers.length)
             for (int i = 0; i < numbers.length; i++) {
                 System.out.println(numbers[i]);
             }
